a7e8d5196ba690148bde40e3f428c212be097bea
sathyawageswara <91704840+sathyawageswara@users.noreply.github.com>
2022-05-13 14:38:17 +0800

    Add files via upload

---
+++ Mini Project/src/router/index.js
@@ -0,0 +1,33 @@
+import Vue from 'vue';
+import VueRouter from 'vue-router';
+import HomePage from '../views/HomePage.vue';
+import Camp from '../views/BarangCamping.vue';
+import Tenda from '../views/Tenda.vue';
+
+Vue.use(VueRouter);
+
+const routes = [
+  {
+    path: '/',
+    name: 'home',
+    component: HomePage,
+  },
+  {
+    path: '/BarangCamping',
+    name: 'BarangCamping',
+    component: Camp,
+  },
+  {
+    path: '/Tenda',
+    name: 'Tenda',
+    component: Tenda,
+  },
+];
+
+const router = new VueRouter({
+  mode: 'history',
+  base: process.env.BASE_URL,
+  routes,
+});
+
+export default router;
