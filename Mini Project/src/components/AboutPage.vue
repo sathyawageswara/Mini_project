b0147f77a21b7521269e33568ef8365b262e7b91
sathyawageswara <91704840+sathyawageswara@users.noreply.github.com>
2022-05-15 15:51:59 +0800

    Update AboutPage.vue

--- Mini Project/src/components/AboutPage.vue
+++ Mini Project/src/components/AboutPage.vue
@@ -13,7 +13,7 @@
         <v-list-item-group v-model="group" active-class="deep-purple--text text--accent-4">
           <v-list-item>
             <v-list-item-icon> <v-icon>mdi-home</v-icon> </v-list-item-icon>
-            <v-btn router-link="/">home</v-btn>
+            <v-btn router-link="/">Home</v-btn>
           </v-list-item>
           <v-list-item>
             <v-list-item-icon>
@@ -30,6 +30,44 @@
         </v-list-item-group>
       </v-list>
     </v-navigation-drawer>
+    <template>
+      <v-app>
+        <v-parallax height="1000" dark src="https://cdn.vuetifyjs.com/images/backgrounds/vbanner.jpg">
+          <v-row align="center" justify="center">
+            <v-col class="text-center" cols="12">
+              <h1 class="text-h4 font-weight-thin mb-4">Campsite</h1>
+              <h4 class="subheading text-center">
+                Phasellus feugiat arcu sapien, et iaculis ipsum elementum sit amet. Mauris cursus commodo interdum. Praesent ut risus eget metus luctus accumsan id ultrices nunc. Sed at orci sed massa consectetur dignissim a sit amet dui.
+                Duis commodo vitae velit et faucibus. Morbi vehicula lacinia malesuada. Nulla placerat augue vel ipsum ultrices, cursus iaculis dui sollicitudin. Vestibulum eu ipsum vel diam elementum tempor vel ut orci. Orci varius natoque
+                penatibus et magnis dis parturient montes, nascetur ridiculus mus.
+              </h4>
+            </v-col>
+          </v-row>
+        </v-parallax>
+        <v-spacer></v-spacer>
+        <v-footer dark padless>
+          <v-card flat tile class="indigo lighten-1 white--text text-center">
+            <v-card-text>
+              <v-btn v-for="icon in icons" :key="icon" class="mx-4 white--text" icon>
+                <v-icon size="24px">
+                  {{ icon }}
+                </v-icon>
+              </v-btn>
+            </v-card-text>
+
+            <v-card-text class="white--text pt-0">
+              Phasellus feugiat arcu sapien, et iaculis ipsum elementum sit amet. Mauris cursus commodo interdum. Praesent ut risus eget metus luctus accumsan id ultrices nunc. Sed at orci sed massa consectetur dignissim a sit amet dui.
+              Duis commodo vitae velit et faucibus. Morbi vehicula lacinia malesuada. Nulla placerat augue vel ipsum ultrices, cursus iaculis dui sollicitudin. Vestibulum eu ipsum vel diam elementum tempor vel ut orci. Orci varius natoque
+              penatibus et magnis dis parturient montes, nascetur ridiculus mus.
+            </v-card-text>
+
+            <v-divider></v-divider>
+
+            <v-card-text class="white--text"> {{ new Date().getFullYear() }} — <strong>Vuetify</strong> </v-card-text>
+          </v-card>
+        </v-footer>
+      </v-app>
+    </template>
   </v-card>
 </template>
 <script>
@@ -38,6 +76,7 @@ export default {
   data: () => ({
     drawer: false,
     group: null,
+    icons: ['mdi-facebook', 'mdi-twitter', 'mdi-linkedin', 'mdi-instagram'],
   }),
 };
 </script>
